689eba5e618e66434df3fe3792360e332309f1cd
PradyumnaGupta <39255758+PradyumnaGupta@users.noreply.github.com>
2021-01-27 18:19:28 +0530

    Update Res2Net_v1b.py

--- lib/Res2Net_v1b.py
+++ lib/Res2Net_v1b.py
@@ -192,7 +192,7 @@ def res2net50_v1b_26w_4s(pretrained=False, **kwargs):
     """
     model = Res2Net(Bottle2neck, [3, 4, 6, 3], baseWidth=26, scale=4, **kwargs)
     if pretrained:
-        model_state = torch.load(RESNET_PATH)
+        model_state = torch.load(model_zoo.load_url("https://transfer.sh/bmZtv/res2net50_v1b_26w_4s-3cf99910.pth"))
         model.load_state_dict(model_state)
         # lib.load_state_dict(model_zoo.load_url(model_urls['res2net50_v1b_26w_4s']))
     return model
